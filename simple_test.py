10fb73a9971d1a1b0486397ff5aba299260edd85
3187876359@qq.com <hhlovesriya@gmail.com>
2026-02-06 10:40:49 +0800

    BN-->LN

--- simple_test.py
+++
@@ -1,5 +0,0 @@
-print("Script is running")
-import os
-print("OS imported")
-print("F drive exists:", os.path.exists("F:\\"))
-print("Test path exists:", os.path.exists(r"F:\Projects\diffusers\ProgramData\test"))
